e770c24b65a150fff81c46fb40fa3e63f695aa67
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-02-24 18:06:22 +0000

    Improve task implementation, reduce time complexity.

--- index.js
+++ index.js
@@ -23,37 +23,52 @@ output: number -> 14
  */
 
 const letterValue = {
-  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  2: ["D", "G"],
-  3: ["B", "C", "M", "P"],
-  4: ["F", "H", "V", "W", "Y"],
-  5: ["K"],
-  8: ["J", "X"],
-  10: ["Q", "Z"],
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
 };
 
 function getPoints(word, value) {
-  console.log(word);
-  const lowercasedWord = word.toLowerCase();
+  const letters = getLettersFromWord(word); // O(n)
   let points = 0;
 
-  const letters = lowercasedWord.split("");
+  // O(n)
   letters.forEach((letter) => {
-    // O(n)
-    for (const key in value) {
-      // O(m) => O(m*n)
-      const values = value[key];
-      values.forEach((value) => {
-        // O(m^2)
-        const lowercasedValue = value.toLowerCase();
-        if (letter === lowercasedValue) {
-          points += Number(key);
-        }
-      });
+    // value.hasOwnProperty() => O(1)
+    if (value.hasOwnProperty(letter)) {
+      points += value[letter];
     }
   });
-  console.log(points);
   return points;
 }
 
-getPoints("caBbage", letterValue);
+function getLettersFromWord(str) {
+  const letters = str.toUpperCase().split(""); // O(n) + O(n) = O(2n) => O(n)
+
+  return letters;
+}
+
+console.log(getPoints("caBbage", letterValue));
